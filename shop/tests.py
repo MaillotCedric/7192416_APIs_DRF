7c1ed6c161de4932eea11a60310ec0c461313993
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-27 11:41:16 +0400

    Update ShopAPITestCase
    ShopAPITestCase contient :
        - l'initialisation de données de test

--- shop/tests.py
+++ shop/tests.py
@@ -4,6 +4,19 @@ from rest_framework.test import APITestCase
 from shop.models import Category
 
 class ShopAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name="Fruits", active=True)
+
+        Category.objects.create(name="Légumes", active=False)
+
+        cls.product = cls.category.products.create(name="Ananas", active=True)
+
+        cls.category.products.create(name="Banane", active=False)
+
+        cls.category_2 = Category.objects.create(name="Épicerie", active=True)
+        cls.product_2 = cls.category_2.products.create(name="Sel", active=True)
+
     def format_datetime(self, value):
         return value.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
@@ -11,10 +24,6 @@ class TestCategory(ShopAPITestCase):
     url = reverse_lazy("category-list") # <basename>-list
     
     def test_list(self): # test si les catégories retournées sont seulement celles qui sont actives
-        category = Category.objects.create(name="Fruits", active=True)
-
-        Category.objects.create(name="Légumes", active=False)
-
         response = self.client.get(self.url)
 
         # vérification du status_code
@@ -27,16 +36,16 @@ class TestCategory(ShopAPITestCase):
                 "name": category.name,
                 "date_created": self.format_datetime(category.date_created),
                 "date_updated": self.format_datetime(category.date_updated)
-            }
+            } for category in [self.category, self.category_2]
         ]
 
         self.assertEqual(response.json(), expected)
     
     def test_create(self):
-        self.assertFalse(Category.objects.exists())
+        category_count = Category.objects.count()
 
-        response = self.client.post(self.url, data={"name": "Tentative"})
+        response = self.client.post(self.url, data={"name": "Nouvelle catégorie"})
 
         self.assertEqual(response.status_code, 405)
 
-        self.assertFalse(Category.objects.exists())
+        self.assertEqual(Category.objects.count(), category_count)
