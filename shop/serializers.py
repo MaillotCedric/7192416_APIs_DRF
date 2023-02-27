163c8633e10eb6dcd2c0f6ed7129060fd9dfc254
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-27 14:18:48 +0400

    Update serializer
    - catégories
        - chaque catégorie va également afficher la liste des produits qui la compose

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,16 +1,23 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from shop.models import Category, Product, Article
-
-class CategorySerializer(ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name", "date_created", "date_updated"]
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "date_created", "date_updated", "name", "category"]
+
+class CategorySerializer(ModelSerializer):
+    products = SerializerMethodField()
+    class Meta:
+        model = Category
+        fields = ["id", "name", "date_created", "date_updated", "products"]
+    
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+
+        return serializer.data
 
 class ArticleSerializer(ModelSerializer):
     class Meta:
